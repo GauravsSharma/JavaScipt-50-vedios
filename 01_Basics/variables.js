70724e5bb7fecfbb4c184f14f4747aacc70cd30a
Gaurav Sharma <113518199+GauravsSharma@users.noreply.github.com>
2023-08-19 11:46:52 +0000

    variables added

---
+++ 01_Basics/variables.js
@@ -0,0 +1,16 @@
+// "use strict"; ===>> for use the latest version js
+
+/* 
+ number;
+ strings;
+ boolean;
+ object;
+ null;   ===> something which is empty
+ undefined;   ====> something which is declared but not initialised
+ bigint
+ symbol ===> are unique;
+
+ type of null is object
+ type of undefined is undefined;
+
+*/
