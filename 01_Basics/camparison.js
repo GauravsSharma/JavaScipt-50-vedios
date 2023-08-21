676e87d08c41439b4ea2ae475e70df7f2081f517
Gaurav Sharma <113518199+GauravsSharma@users.noreply.github.com>
2023-08-21 11:27:32 +0000

    comparison

---
+++ 01_Basics/camparison.js
@@ -0,0 +1,13 @@
+// console.log("2">=2); "2" will be converted into a number 
+// console.log("2"==2);     same
+// console.log("2"<=2);     same
+  
+
+console.log(null>=2);  //unexpected
+// console.log(null==2);   in case of == null will be converted into a 0;
+console.log(null<=2);  //unexpected
+
+console.log(undefined>=0);   // gives you false
+console.log(undefined>=0);   // gives you false
+console.log(undefined>=0);   // gives you false
+
